c9d5471b867789d8f0215687fc1178cc9d2b6d6b
Chuanjian Wang <me@ckeyer.com>
2019-12-22 21:37:20 +0800

    fix ui

--- ui/src/router/index.ts
+++ ui/src/router/index.ts
@@ -1,6 +1,6 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+import Home from '@/views/Home.vue'
 
 Vue.use(VueRouter)
 
@@ -11,12 +11,15 @@ const routes = [
     component: Home
   },
   {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    path: '/',
+    component: () => import(/* webpackChunkName: "about" */ '@/views/layout/Logined.vue'),
+    children: [
+      {
+        path: 'about',
+        name: 'about',
+        component: () => import(/* webpackChunkName: "about" */ '@/views/About.vue')
+      }
+    ]
   }
 ]
 
